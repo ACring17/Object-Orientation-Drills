3e2bc9525bbeff005015284bfe285b60a4156a87
Andrew Cring <amcring1@gmail.com>
2022-03-05 14:27:53 -0700

    Solution for question 14

--- lab.js
+++ lab.js
@@ -243,7 +243,23 @@ wiz1.castSpell()
 */
 
 //Code Here
-
+class Phone {
+    constructor(brand, model, storage, color, price) {
+        this.brand = brand
+        this.model = model
+        this.storage = storage
+        this.color = color
+        this.price = price
+        this.sold = false
+    }
+    sell() {
+        this.sold = true
+        console.log(`${this.brand} ${this.model} has been sold.`)
+    }
+    changePrice(newPrice) {
+        this.price = newPrice
+    }
+}
 
 /*
     Next make three new phone instances using your class.
